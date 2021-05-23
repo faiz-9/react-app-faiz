8eb248c74743e58476f82d6c7684cba580288619
dikshant rajput <dikshantraj2001@gmail.com>
2021-05-23 18:58:19 +0530

    removed extra code

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Common from "./Common";
-import { NavLink } from "react-router-dom";
 let imgg = "https://source.unsplash.com/collection/wallpaper/800x600";
 const Home = () => {
   return (
